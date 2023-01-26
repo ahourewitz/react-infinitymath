2b5cc6374db223bb514ca5301093ad1092fc70ae
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2023-01-26 19:10:22 +0100

    Improves Scaling of Article Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './App.scss';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LayoutMainMenu from "./pages/LayoutMainMenu";
 import Home from "./pages/Home";
