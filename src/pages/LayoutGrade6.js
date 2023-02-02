43ad57cc78202f178297fe26b3d5bfada7016617
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2023-02-02 20:44:56 +0100

    Improve Title

--- src/pages/LayoutGrade6.js
+++ src/pages/LayoutGrade6.js
@@ -8,20 +8,14 @@ const LayoutGrade6 = () => {
     return (
         <>
             <nav className={"layout_articles"}>
-                <ul>
-                    <li>
                         <ArticleCard title={"Titre Article 2"}
                                      short_description={"Description Article 2"}
                                      img_src={article2_card_poster}
                                      link={"Article2"}/>
-                    </li>
-                    <li>
                         <ArticleCard title={"Titre Article 1"}
                                      short_description={"\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
                                      img_src={article1_card_poster}
                                      link={"Article1"}/>
-                    </li>
-                </ul>
             </nav>
             <Outlet/>
         </>
